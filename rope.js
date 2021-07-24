da32d7cc26dfaa1daa41f92ab8bbae3aa172e437
VarshithGudala <84899041+VarshithGudala@users.noreply.github.com>
2021-07-24 19:15:28 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,19 @@
+class rope{
+	constructor()
+	{
+
+	//create rope constraint here
+
+	}
+
+
+
+    //create display() here
+	display(){
+		var pointA = rope.bodyA.position;
+		var pointB = rope.bodyB.position;
+
+		strokeWeight(2);
+	} 
+
+}
